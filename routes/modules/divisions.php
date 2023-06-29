3b224c5c2ef7f9428fbb5c2ed46fd314b50ddaf0
Enea Dhack <enea.so@live.com>
2023-06-29 15:00:40 -0500

    feat(route): add division module

---
+++ routes/modules/divisions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author enea dhack <enea.so@live.com>
+ */
+
+// Routes ${host}/api/
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+use Mandu\Core\Division\Infrastructure\Controllers as Division;
+
+Route::post('create', Division\CreateDivisionController::class)->name('api.divisions.create');
