19e734019af6ac363eba29229147f7b3bb2e2791
Enea Dhack <enea.so@live.com>
2023-06-29 14:07:00 -0500

    feat: add base

---
+++ core/Shared/infrastructure/Controller.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author enea dhack <enea.so@live.com>
+ */
+
+declare(strict_types=1);
+
+namespace Mandu\Core\Shared\infrastructure;
+
+use App\Http\Controllers\Controller as IlluminateController;
+
+abstract class Controller extends IlluminateController
+{
+
+}
---
+++ core/Shared/infrastructure/FormRequest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author enea dhack <enea.so@live.com>
+ */
+
+declare(strict_types=1);
+
+namespace Mandu\Core\Shared\infrastructure;
+
+use Illuminate\Foundation\Http\FormRequest as IlluminateFormRequest;
+
+abstract class FormRequest extends IlluminateFormRequest
+{
+
+}
