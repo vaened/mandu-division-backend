9917821b94beb5d2c79808e9e95cadb343382b96
Enea Dhack <enea.so@live.com>
2023-06-29 19:07:31 -0500

    build: configure test env

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,40 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Testing\TestResponse;
+use function implode;
+use function Lambdish\Phunctional\map;
+use function route;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, WithoutMiddleware, RefreshDatabase;
+
+    public function doPost(string $name, array $data = [], array $headers = []): TestResponse
+    {
+        return parent::post(route($name), $data, $headers);
+    }
+
+    public function doDelete(string $name, array $data = [], array $headers = []): TestResponse
+    {
+        return parent::delete(route($name, $data), $data, $headers);
+    }
+    public function doPut(string $name, array $data = [], array $headers = []): TestResponse
+    {
+        return parent::put(route($name, $data), $data, $headers);
+    }
+
+    public function doGet(string $name, array $data = [], array $headers = []): TestResponse
+    {
+        $route = route($name, $data);
+        $params = implode('&', map(fn(mixed $value, string $key) => "$key=$value", $data));
+
+        return parent::get(
+            !empty($params) ? "$route?$params" : $route,
+            $headers
+        );
+    }
 }
