2f9e9be5a943c1d5e6e947f58f9e611e8d12bc8e
Enea Dhack <enea.so@live.com>
2023-07-01 11:57:20 -0500

    improve seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,15 +5,34 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Mandu\Core\Division\Domain\Division;
+use function collect;
+use function fake;
 use function random_int;
+use function range;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Division::factory(40)->has(
-            Division::factory(random_int(0, 4)),
-            'subdivisions'
-        )->create();
+        $parentKeys = Division::factory(10)->create()->map($this->keys());
+
+        collect(range(1, 20))
+            ->each(fn() => Division::factory()
+                ->state(fn(array $attributes) => [
+                    'parent_division_id' => fake()->randomElement($parentKeys),
+                ])
+                ->has(
+                    Division::factory(random_int(1, 3)),
+                    'subdivisions'
+                )
+                ->create([
+                    'parent_division_id' => fake()->randomElement($parentKeys),
+                ])
+            );
+    }
+
+    private function keys(): callable
+    {
+        return static fn(Division $division) => $division->id;
     }
 }
